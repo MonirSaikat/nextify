3a8fd8db4b031141b257d3feee5afdbf4568bc9c
Monir Saikat <118444503+MonirSaikat@users.noreply.github.com>
2024-03-07 00:51:46 +0600

    before useFormik

--- src/components/posts/posts.jsx
+++ src/components/posts/posts.jsx
@@ -4,46 +4,15 @@ import React, { useContext, useState } from "react";
 import styles from "@/components/posts/posts.module.css";
 import toast from "react-hot-toast";
 import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from 'yup';
 
 export default function Posts() {
   const { posts, removePost, addPost, postById, updatePost } = useContext(PostContext);
-  const [title, setTitle]                                     = useState("");
-  const [isEdit, setIsEdit]                                   = useState(false);
-  const [description, setDescription]                         = useState("");
   const [editId, setEditId]                                   = useState(null);
 
   const editPost = (postId) => {
-    const post = postById(postId);
-    setTitle(post.title);
-    setIsEdit(true);
-    setDescription(post.description);
+    postById(postId);
     setEditId(postId);
-  };
-
-  const handleEditPost = e => {
-    e.preventDefault();
-    updatePost(editId, {title, description});
-    setTitle('');
-    setDescription('');
-    setEditId(null);
-    setIsEdit(false);
-  }
-  
-  const handleAddPost = (e) => {
-    e.preventDefault();
-    if('' == title || '' == description) {
-      toast.error('Please enter a title and description');
-      return false;
-    }
-    
-    const post = {
-      id: Date().toString(),
-      title, description
-    }
-
-    addPost(post);
-    setTitle('');
-    setDescription('');
   };
 
   const initialValues = {
@@ -51,37 +20,58 @@ export default function Posts() {
     description: '',
   };
 
-  const handleSubmit = (values) => {
-    console.log(values);
+  const handleSubmit = (values, { resetForm }) => {
+    const post = {
+      id: Date().toString(),
+      title: values.title, 
+      description: values.description
+    }
+
+    if(editId) {
+      delete post.id;
+      const foundPost = postById(editId);
+      initialValues.title = foundPost.title;
+      initialValues.description = foundPost.description;
+      updatePost(editId, post);
+      setEditId(null);
+    } else {
+      addPost(post);
+    }
+
+    resetForm();
   }
+
+  const postSchema = Yup.object().shape({
+    title: Yup.string().required(),
+    description: Yup.string().required()
+  });
   
   return (
     <div className={styles.container}>
-      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Formik enableReinitialize initialValues={initialValues} onSubmit={handleSubmit} validationSchema={postSchema}>
         <Form className={styles.form}>
-          <input
+          <Field
             type="text"
             className={styles.input}
             placeholder="Title"
-            name="title"
+            name="title" 
           />
           <ErrorMessage name="title" component="div" />
-          <textarea
+          <Field
+            as="textarea"
             placeholder="Description"
             name="description"
             className={styles.input}
-          ></textarea>
+          ></Field>
           <ErrorMessage name="description" component="div" />
           <button type="submit" className={styles.button}>
-            {isEdit ? "Edit Post" : "Add New Post"}
+            {editId ? "Edit Post" : "Add New Post"}
           </button>
         </Form>
-      </Formik>
-      <form className={styles.form} onSubmit={isEdit ? handleEditPost : handleAddPost}>
-      </form>
+      </Formik> 
       <h1 className={styles.heading}>Posts</h1>
 
-      {posts ? (
+      {(posts && posts.length) ? (
         posts.map((post) => (
           <div key={post.id} className={styles.post}>
             <h3 className={styles.title}>{post.title}</h3>
